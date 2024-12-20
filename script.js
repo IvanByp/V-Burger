c0fa0e81be83bec788110f40d55e10e3be6658f5
IvanBayu <97517799+IvanByp@users.noreply.github.com>
2024-12-20 13:57:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//Navbar
+document.addEventListener('DOMContentLoaded', (event) => {
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+});
+//Dark Mode
+document.addEventListener('DOMContentLoaded', () => {
+    let darkmode = document.querySelector('#darkmode');
+
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon', 'bx-sun');
+            document.body.classList.add('active');
+        } else {
+            darkmode.classList.replace('bx-sun', 'bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+});
+// Scroll Reveal
+const sr= ScrollReveal({
+    origin: 'top',
+    distance: '4opx',
+    duration: 2000,
+    reset: true
+})
+document.addEventListener('DOMContentLoaded', () => {
+    ScrollReveal().reveal('.home-text, .home-img, .about-img, .about-text, .box, .s-box, .connect-text, .btn, .contact-box', {
+        interval: 200
+    });
+});
